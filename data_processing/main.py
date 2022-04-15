42656a9a2e870ce7ea87d44aa8bb62d7168cf23c
jose-1180943 <94014757+jose-1180943@users.noreply.github.com>
2022-04-15 20:52:43 +0100

    Update to tokenization

--- data_processing/main.py
+++ data_processing/main.py
@@ -1,10 +1,11 @@
-from string import punctuation
 from pymongo import MongoClient
 import hashlib
 import nltk
 from nltk.corpus import stopwords
-#from sklearn.feature_extraction.text import TfidfVectorizer
+from regex import W
+from sklearn.feature_extraction.text import TfidfVectorizer
 import re
+import pandas as pd
 
 # connect to the database
 def load_db():
@@ -23,73 +24,95 @@ def populate_db(data, collection):
             'features': o['features']
         })
 
-
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('wordnet')
 
 def main():
+    db = load_db()
+    raw_data = db.RAW_DATA
+    data =  raw_data.find()
+
     # Stop words
     stop_words = stopwords.words('english')
 
-    db = load_db()
-    raw_data = db.RAW_DATA
-
-    # Tokenize texts
     tokenizer = nltk.tokenize.TweetTokenizer(
         preserve_case=False, reduce_len=True, strip_handles=True, match_phone_numbers=False)
-    stemmer = nltk.stem.PorterStemmer()
-    #lemmer = nltk.stem.WordNetLemmatizer()
+    #stemmer = nltk.stem.PorterStemmer()
+    lemmer = nltk.stem.WordNetLemmatizer()
 
+    """ texts = []
+    for tweet in data:
+        texts.append(tweet['text']) """
 
     processed_data = []
-    for tweet in raw_data.find():
+    
+    for tweet in data:
         text = tweet['text']
+
+        # remove stock market tickers like $GE
+        text = re.sub(r'\$\w*', '', text)
+
+        # remove old style retweet text "RT"
+        text = re.sub(r'^RT[\s]+', '', text)
 
         #Remove links
         text = re.sub(r'http\S+', '', text)
 
         #Seperate sentences
         sentences = nltk.tokenize.sent_tokenize(text, language='english')
-
+        
         for sentence in sentences:
-            print('NEW SENTENCE:\n\t', sentence)
-
+            
+            tokenized_sentence = []
             #Tokenize sentence            
             tokenized_sentence = tokenizer.tokenize(sentence)
 
-            #Remove # 
-            for token in tokenized_sentence:
-                if token.startswith('#'):
-                    token = token[1:]
-                    
-            filtered_sentence = [w[1:] for w in tokenized_sentence if token.startswith('#') else w]
+            #Remove #
+            filtered_sentence = [w[1:] if w.startswith('#') else w for w in tokenized_sentence ]
 
             # Remove stop words
-            filtered_sentence = [w for w in tokenized_sentence if not w in stop_words]
+            filtered_sentence = [w for w in filtered_sentence if not w in stop_words]
 
-            filtered_sentence = [w for w in tokenized_sentence if not w in punctuation]
+            # Remove numbers
+            filtered_sentence = [w for w in filtered_sentence if not re.match(r'^([,.\d]*)([,.]?\d*)$', w)]
+
+            # Remove punctuation
+            filtered_sentence = [w for w in filtered_sentence if not re.match(r'[^\w\s]', w)]
 
             #Check is sentence is empty
             if not filtered_sentence:
                 continue
 
             # Stem words
-            stemmed_sentence = [stemmer.stem(token) for token in filtered_sentence]
+            #stemmed_sentence = [stemmer.stem(token) for token in filtered_sentence]
+            final_sentence = [lemmer.lemmatize(token) for token in filtered_sentence]
 
             # Rewrite new string
-            sentence = ' '.join(stemmed_sentence)
+            sentence = ' '.join(final_sentence)
 
             # Generate unique hash
             id = hashlib.md5(sentence.encode('unicode-escape')).hexdigest()
 
-            o = {'post_id':tweet['_id'], '_id': id, 'text': stemmed_sentence, 'features': []}
-            print('\t',o)
-            processed_data.append(o)
+            print(sentence)
 
+            o = {'post_id':tweet['_id'], '_id': id, 'text': final_sentence, 'features': []}
+            #texts.append(sentence)
+            
+            #print('\t',o)
+            
+            processed_data.append(o)
+    
     populate_db(processed_data, db.PROCESSED_DATA)
 
-    #tdidf = TfidfVectorizer(min_df=2, max_df=.9, ngram_range=(1, 2))
-    #features = tdidf.fit_transform(texts)
+    """ vectorizer = TfidfVectorizer(min_df=4, max_df=.7, ngram_range=(1,2))
+
+    features = vectorizer.fit_transform(texts)
+
+    print(vectorizer.get_stop_words())
+
+    df = pd.DataFrame(features.todense(), columns=vectorizer.get_feature_names_out()) """
+    #df.to_csv('a.csv')
 
 if __name__ == "__main__":
     main()
